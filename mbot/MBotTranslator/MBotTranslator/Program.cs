6cb3f17ef8ebe27c345e7828abda694741d95b54
DAJEAtSchool <daniel.jessner@htl-saalfelden.at>
2024-04-18 14:57:01 +0200

    updated documentation + added mbot translator

---
+++ mbot/MBotTranslator/MBotTranslator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace MBotTranslator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Translator trans = new Translator();
+
+            Console.WriteLine("Rechtskurve");
+            Console.WriteLine(trans.Translate(.5, 1));
+            Console.WriteLine();
+
+            Console.WriteLine("Linkskurve");
+            Console.WriteLine(trans.Translate(-.5, 1));
+            Console.WriteLine();
+
+            Console.WriteLine("Rückwärts rechtskurve");
+            Console.WriteLine(trans.Translate(.5, -1));
+            Console.WriteLine();
+
+            Console.WriteLine("Rückwärts linkskurve");
+            Console.WriteLine(trans.Translate(-.5, -1));
+            Console.WriteLine();
+
+            Console.WriteLine(trans.Translate(.1, .9));
+            Console.WriteLine();
+
+            Console.WriteLine(trans.Translate(-.3, -.4));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ mbot/MBotTranslator/MBotTranslator/Translator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBotTranslator
+{
+    internal class Translator
+    {
+        public string Translate(double x, //Left or Right
+            double y) //Straight or Back
+        {
+            int div = (int)Math.Round((100 * y) * x);
+            int left;
+            int right;
+
+            if (x > 0)
+            {
+                if (y < 0)
+                {
+                    right = (int)Math.Round((100*y) + Math.Abs(div));
+                    left = (int)Math.Round(100 * y);
+                }
+                else
+                {
+                    right = (int)Math.Round((100*y) - Math.Abs(div));
+                    left = (int)Math.Round(100 * y);
+                }
+            }
+            else
+            {
+                if (y < 0)
+                {
+                    left = (int)Math.Round((100 * y) + Math.Abs(div));
+                    right = (int)Math.Round(100 * y);
+                }
+                else
+                {
+                    left = (int)Math.Round((100 * y) - Math.Abs(div));
+                    right = (int)Math.Round(100 * y);
+                }
+            }
+
+            return $"{left};{right}";
+        }
+    }
+}
